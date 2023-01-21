e22977b8a83ea0ee625b341cdc45ac577be12d5f
imhjnoh <imhjnoh@gmail.com>
2023-01-22 01:32:45 +0900

    set up google map & add markers

---
+++ src/CustomMap.ts
@@ -0,0 +1,66 @@
+import { Company } from './Company';
+import { User } from './User'
+
+// Instruction to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    }
+    markerContent(): string;
+}
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+    private element: HTMLElement | null;
+    constructor(divId: string) {
+        this.element = document.getElementById(divId)
+        if(this.element){
+        this.googleMap = new google.maps.Map(this.element, {
+        zoom: 1,
+        center: {
+            lat: 0,
+            lng: 0
+        }
+        });}
+    }
+
+    // mappable은 TS가 자동으로 User와 Company모두가 가진 프로퍼티만 가지게 된다.
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        })
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+            
+            infoWindow.open(this.googleMap, marker);
+        })
+    }
+
+    // addUserMarker(user: User): void {
+    //     new google.maps.Marker({
+    //         map: this.googleMap,
+    //         position: {
+    //             lat: user.location.lat,
+    //             lng: user.location.lng
+    //         }
+    //     })
+    // }
+
+    // addCompanyMarker(company: Company): void {
+    //     new google.maps.Marker({
+    //         map: this.googleMap,
+    //         position: {
+    //             lat: company.location.lat,
+    //             lng: company.location.lng
+    //         }
+    //     })
+    // }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-/// <reference types="@types/google.maps" />
-import { Company } from './Company';
+// /// <reference types="@types/google.maps" />
 import { User } from './User'
+import { CustomMap } from "./CustomMap";
+import { Company } from './Company';
 
-const user = new User()
+// 아래와 같이 작성하면 누구나 Map객체를 사용해 다른 메소드를 사용할 수 있고,
+// 그럴 경우 앱이 정상적으로 작동하지 않게 될 수 있다.
+// 이러한 잠재적인 위험요소를 제거해보자.
+// const mapDiv = document.getElementById('map');
+// if (mapDiv) {
+//   new google.maps.Map(mapDiv, {
+//     zoom: 1,
+//     center: {
+//       lat: 0,
+//       lng: 0
+//     }
+//   });
+// }
 
-console.log(user);
-
-const company = new Company()
-
-console.log(company);
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
